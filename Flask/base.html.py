332406eee8129f6ea169576333e81bb33f08a059
unknown <agnieszka.gruszecka@gmail.com>
2020-10-16 11:56:10 +0200

    Flask files, pickled files etc.

---
+++ Flask/base.html.py
@@ -0,0 +1,7 @@
+{% block title%}Wpisz opinie{% endblock %}
+{% block content %}
+<form action = "#", method = "post">
+    <p><input type='text'/></p>
+    <p><input type='submit' value
+</form>
+{% endblock %}
