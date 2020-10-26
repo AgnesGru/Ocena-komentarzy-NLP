e29ba52ed9b2c9d58d935fc1b7e48d30922296c6
unknown <agnieszka.gruszecka@gmail.com>
2020-10-26 12:59:53 +0100

    cleaned

--- Flask/minimal_app.py
+++ Flask/minimal_app.py
@@ -2,13 +2,6 @@ from flask import Flask, request, current_app, render_template, jsonify
 app = Flask(__name__)  #nowa instancja klasy Flask
 from loaded_pickle_main import get_string, change_into_string
 
-
-# @app.route('/')  # endpointy we Flasku
-# def hello_world():
-#     # return 'Hello, World!'
-#     return render_template("base.html")
-
-
 @app.route('/', methods = ['POST', 'GET'])
 def send_sentiment():
 #     writen_opinion = request.args.get('opinion')  # opinia wysłana
