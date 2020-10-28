1d6d48b1b5e1775ad9c4c536933ff93a707747df
unknown <agnieszka.gruszecka@gmail.com>
2020-10-28 15:09:34 +0100

    wymęczony front-end

--- Flask/minimal_app.py
+++ Flask/minimal_app.py
@@ -26,7 +26,7 @@ def send_sentiment():
 #         # print(data)
 #         return jsonify(data) #str(result)
 #     else:
-#         return render_template("base.html")
+#         return render_template("index.html")
 
 
 if __name__ == "__main__":
